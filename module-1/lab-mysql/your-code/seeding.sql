292716f28294bfb5f34dbcefe5face9d1e4c6d99
Antoniosm88 <63240398+Antoniosm88@users.noreply.github.com>
2020-06-09 20:27:26 +0200

    Challenge3

---
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -0,0 +1,23 @@
+INSERT INTO `lab-mysql`.`Customers` (`idcustomers`, `name`, `phone_number`, `email`, `address`, `city`, `postal_code`) VALUES ('10001', 'Pablo Picasso','+34 636 17 63 82','-','Paseo de la Chopera, 14','Madrid','28045');
+INSERT INTO `lab-mysql`.`Customers` (`idcustomers`, `name`, `phone_number`, `email`, `address`, `city`, `postal_code`)VALUES ('20001', 'Abraham Lincoln	', '+34 644 17 63 82	', '-', '120 SW 8th St', 'Miami', '33130');
+INSERT INTO `lab-mysql`.`Customers`(`idcustomers`, `name`, `phone_number`, `email`, `address`, `city`, `postal_code`) VALUES ('30001', 'Napoléon Bonaparte', '+34 646 17 63 82', '-', '40 Rue du Colisée', 'Paris', '75007');
+
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('1', 'RKXVNNIHLVVZOUB2M', 'Volkswagen', 'Tiguan', '2018', 'Blue');
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('2', 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', '2019', 'Red');
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('3', 'RKXVNNIHLVVZOUB4M', 'Ford	', 'Fusion', '2014', 'White');
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('4', 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4	', '2019', 'Silver\nSilver\n');
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('5', 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', '2019', 'Gray');
+INSERT INTO `lab-mysql`.`Cars` (`idCars`, `VIN`, `manufacturer`, `model`, `year`, `color`) VALUES ('6', 'HKNDGS7CU31E9Z7JW', 'Volvo', 'V60 Cross Country', '2019', 'CassaWhite');
+
+INSERT INTO `lab-mysql`.`Invoices` (`idInvoice`) VALUES ('852399038');
+INSERT INTO `lab-mysql`.`Invoices` (`idInvoice`) VALUES ('731166526');
+INSERT INTO `lab-mysql`.`Invoices` (`idInvoice`) VALUES ('271135104');
+ 
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00001', 'Petey Cruiser', 'Madrid');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00002', 'Anna Sthesia', 'Barcelona');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00003', 'Paul Molive', 'Berlin');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00004', 'Gail Forcewind', 'Paris');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00005', 'Paige Turner', 'Miami');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00006', 'Bob Frapples', 'Mexico City');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00007', 'Walter Melon', 'Amsterdam');
+INSERT INTO `lab-mysql`.`Salespersons` (`idstaff`, `name`, `store`) VALUES ('00008', 'Shonda Leer', 'São Paulo'); 
